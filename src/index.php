bbc531f48c26e14dea529f1ca526453ef2be564c
Benjamin-51 <babilliot.benjamin@gmail.com>
2021-10-18 12:31:50 +0200

    first commit

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '\..\vendor\autoload.php';
+
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+
+$bessie->setTongue('U');
+
+$output = $bessie->say();
+
+echo $output;
